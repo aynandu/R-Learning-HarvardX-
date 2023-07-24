54af18747163b62638adc65097d03b6d31373dc4
Nandu AY <nandu.a.y@hotmail.com>
2023-07-24 22:18:55 +0530

    basics

---
+++ objects.r
@@ -0,0 +1,11 @@
+# assigning values to variables
+a<-1
+b<-1
+c<--1
+d=3
+
+# <- is the assignment operator in R and = also works
+# solving the quadratic equation
+(-b + sqrt(b^2 - 4*a*c))/(2*a)
+(-b - sqrt(b^2 - 4*a*c))/(2*a)
+
